28df0fe4463ded83fdac2e44caeb74c2850412bb
lhjnano <lhjnano@gmail.com>
2018-01-26 16:20:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ git clone git://git.videolan.org/x264.git
 
 echo BUILD X264
 cp x264_build_android.sh ./x264/
-./x264/x264_build_android.sh
+cd ./x264
+./x264_build_android.sh
 
 echo BUILD FFMPEG
+cd ..
 ./build_android.sh
 
 @echo Complete install x264
